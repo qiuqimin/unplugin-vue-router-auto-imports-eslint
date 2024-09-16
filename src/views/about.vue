bb70e8ccf6ad84a790c48747a1fa3de5ce2db95f
qiu.qimin <qimin.qiu@outlook.com>
2024-09-16 14:25:24 +0800

    first commit

---
+++ src/views/about.vue
@@ -0,0 +1,26 @@
+<script setup>
+definePage({
+  meta: {
+    key: 'value'
+  }
+})
+
+const route = useRoute()
+console.log('route.meta.key:', route.meta.key)
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+import AutoImport from 'unplugin-auto-import/vite'
+
+import VueRouter from 'unplugin-vue-router/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    VueRouter({
+      routesFolder: 'src/views'
+    }),
+    // ⚠️ Vue must be placed after VueRouter()
+    vue(),
+    AutoImport({
+      imports: [VueRouterAutoImports],
+      eslintrc: {
+        enabled: true
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
